c4e5e42aec7eaca6b374812d4b51dc6bb55f0db2
ZaminAhammed <zaminahammed801@gmail.com>
2025-06-02 19:50:01 +0530

    Update and rename ModelsEvaluation.py to models_evaluation.py

--- src/models_evaluation.py
+++ src/models_evaluation.py
@@ -20,35 +20,79 @@ from sklearn.model_selection import LeaveOneOut, StratifiedKFold, KFold, Stratif
 from sklearn.metrics import roc_curve, auc, roc_auc_score, RocCurveDisplay
 from sklearn.metrics import precision_recall_curve, confusion_matrix
 
+def main():
+    print (f"Training Set supplied : '{sys.argv[1]}'")
+    print (f"Test Set supplied : '{sys.argv[2]}'")
+
+    #Models to be evaluated (hyperamaters are set based on GridSearch performed using TrainingSet_1)
+    model_1 = LogisticRegression(C=2.0, max_iter=50, penalty='l1', solver='liblinear')
+    model_2 = SVC(kernel='linear', probability=True)
+    model_3 = RandomForestClassifier(random_state=1)
+    model_4 = xgb.XGBClassifier(objective="binary:logistic", seed=1, learning_rate=0.75, n_estimators=None, max_depth=None)
+    model_5 = TabPFNClassifier()
+    models = [model_1, model_2, model_3, model_4, model_5]
+    
+    #Data importing
+    train = pd.DataFrame(sys.argv[1])
+    test = pd.DataFrame(sys.argv[2])
+
+    #Cleaning data
+    train_X, train_Y = clean_data(train)
+    test_X, test_Y = clean_data(test)
+
+    #Training
+    best_model = model_eval(models, train_X, train_Y)   
+    shap_eval(model_4, train_X, train_Y)
+    PR_curves(models, train_X, train_Y)
+    learning_curves(models, train_X, train_Y)
+
+    #Classifying unlabelled test set
+    results = test(best_model, train_X, train_Y, test_X)
+    results.to_csv(sys.argv[3], index=False)
+
+    print (f"The final results have been saved to : '{sys.argv[3]}'\n")    
+
 def clean_data(data, standardizer = StandardScaler()):
     data.dropna(inplace=True)
     X = data.select_dtypes({'float64','int64'})
-    X.drop(columns={'Target'})
-    Y = data.Target
+    if 'Target' in X.columns:
+        X.drop(columns={'Target'})
+        Y = data.Target
+    else:
+        Y = {}
     X_stand = standardizer.fit_transform(X)
     X_df = pd.DataFrame(X_stand, columns = X.columns)
     return X_df, Y
 
-def model_eval(models, X, Y, in_data, cv = KFold(n_splits=10, random_state=1, shuffle=True),
+def model_eval(models, X, Y, cv = KFold(n_splits=10, random_state=1, shuffle=True),
                scoring = ['accuracy', 'f1', 'precision', 'recall', 'roc_auc'], n_jobs = -1, error_score = 'raise'):
     result = pd.DataFrame({'Model': ['LogisticRegression', 'SVM', 'RandomForest', 'XGBoost', 'TabFPN']})
+    f1_scores = []
     for y in scoring:
         kfold = []
         for x in models:
             score = cross_val_score(x, X, Y, scoring=y, cv=cv, n_jobs=n_jobs, error_score=error_score)
             kfold.append('%.3f (%.3f)' % (np.mean(score), np.std(score)))
+            if y == 'f1':
+                f1_scores.append(np.mean(score))
         result[y] = kfold
-    print(f'Performance of Models with only {in_data} data')
+    print(f'Performance of Models:')
     print(result.to_string())
+    best_f1 = max(f1_scores)
+    for i in range(5):
+        if f1[i] == best_f1:
+            best_model = models[i]
+    return best_model
+        
 
-def shap_find(model, X, Y, in_data):
+def shap_eval(model, X, Y):
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
     model.fit(X_train, y_train)
     explainer = shap.Explainer(model)
     shap_values = explainer(X_test)
-    shap.summary_plot(shap_values, X_test, plot_type="bar", title=f'SHAP summary plot for XGB with {in_data} data')
+    shap.plots.beeswarm(shap_values, X_test, max_display=X.shape[1])
 
-def PR_curve(models, X, Y, in_data):
+def PR_curves(models, X, Y):
     fig, axis = plt.subplots(2, 3, figsize=(15, 10))
     mods = ["LR", "SVC", "RF", "XGB", "TabFPN"]
     axes = [axis[0, 0], axis[0, 1], axis[0,2], axis[1, 0], axis[1, 1]]
@@ -61,12 +105,13 @@ def PR_curve(models, X, Y, in_data):
         axes[i].plot(recall, precision, label=f'PR Curve (AUC = {auc_score:.2f})')
         axes[i].set_xlabel('Recall')
         axes[i].set_ylabel('Precision')
-        axes[i].set_title(f'PR Curve of {mods[i]} for {in_data} data')
+        axes[i].set_title(f'PR Curve of {mods[i]} for')
         axes[i].legend()
     fig.delaxes(ax=axis[1, 2])
+    fig.savefig(f'PR_Curves.jpeg')
     fig.show()
 
-def learning_curves(models, X, Y, in_data):
+def learning_curves(models, X, Y):
     fig, axis = plt.subplots(2, 3, figsize=(15, 10))
     mods = ["LR", "SVC", "RF", "XGB", "TabFPN"]
     axes = [axis[0, 0], axis[0, 1], axis[0, 2], axis[1, 0], axis[1, 1]]
@@ -85,38 +130,21 @@ def learning_curves(models, X, Y, in_data):
         axes[i].fill_between(sizes, mean_testing + std_testing, mean_testing - std_testing, color="g", alpha=0.2)
         axes[i].set_xlabel("Training set size")
         axes[i].set_ylabel("Accuracy score")
-        axes[i].set_title(f'Learning Curve of {mods[i]} for {in_data} data')
+        axes[i].set_title(f'Learning Curve of {mods[i]}')
         axes[i].legend(loc="best")
     fig.delaxes(ax=axis[1, 2])
-    fig.savefig(f'Learning_Curve{in_data}.jpeg')
+    fig.savefig(f'Learning_Curves.jpeg')
     fig.show()
 
-#Models to be evaluated
-model_1 = LogisticRegression(C=2.0, max_iter=50, penalty='l1', solver='liblinear')
-model_2 = SVC(kernel='linear', probability=True)
-model_3 = RandomForestClassifier(bootstrap=False, min_samples_leaf=4, min_samples_split=10, n_estimators=50, random_state=1)
-model_4 = xgb.XGBClassifier(objective="binary:logistic", seed=1,
-                            learning_rate=0.75, n_estimators=None, max_depth=None)
-model_5 = TabPFNClassifier()
-models = [model_1, model_2, model_3, model_4, model_5]
+def test(model, train_X, train_Y, test):
+    model.fit(train_X_stand, train_Y)
+    probs = model.predict_proba(test_X_stand)
+    yes, no = [], []
+    for x in probs:
+        no.append(x[0])
+        yes.append(x[1])
+    test_results = pd.DataFrame({'Gene':test.Gene, 'Yes':yes, 'No':no})
+    return test_results
 
-#Data importing
-data1 = pd.read_csv("RPKM_Trans_BeforeRemoving.csv")
-data2 = pd.read_csv("RPKM_Integ_BeforeRemoving.csv")
-
-X1, Y1 = clean_data(data1)
-X2, Y2 = clean_data(data2)
-X2.drop(columns={'BestPocket_Probability'}, inplace=True)
-
-#model_eval(models, X1, Y1, "Transcriptomics")
-#model_eval(models, X2, Y2, "Integrated")
-
-#shap_find(model_4, X1, Y1, "Transcriptomics")
-#shap_find(model_4, X2, Y2, "Integrated")
-
-#PR_curve(models, X1, Y1, "Transcriptomics")
-#PR_curve(models, X2, Y2, "Integrated")
-
-learning_curves(models, X1, Y1, "Transcriptomics")
-learning_curves(models, X2, Y2, "Integrated")
-
+if __name__ == "__main__":
+    main()
